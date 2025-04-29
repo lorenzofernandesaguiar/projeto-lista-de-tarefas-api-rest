694943049dc62b412656879942934facaa1f88cf
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2025-04-29 11:23:07 -0300

    Adiciona arquivos básicos
    Arquivos básicos foram adicionados. Esses arquivos estão relacionados com a API REST do projeto Lista de tarefas.

---
+++ src/main/java/com/projetolistadetarefasapirest/ProjetolistadetarefasapirestApplication.java
@@ -0,0 +1,13 @@
+package com.projetolistadetarefasapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetolistadetarefasapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetolistadetarefasapirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetolistadetarefasapirest/ProjetolistadetarefasapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetolistadetarefasapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetolistadetarefasapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
